a1999a7de03b4a76543ab892a76b3d38e573c1f4
Maissack <franckzhangpro@gmail.com>
2023-12-27 12:49:43 +0100

    :sparkles: ADD functions replace text

--- js/replace_text.js
+++ js/replace_text.js
@@ -1,36 +1,26 @@
-var currentYear = new Date().getFullYear();
-        document.getElementById('currentYear').textContent = currentYear;
-    
-
-
-        
-
-
-    // Variable globale pour stocker le projet actuellement affiché
     var currentProject = '';
 
-    // Fonction pour afficher la description du projet
     function showProjectDescription(project) {
         var presentationDiv = document.querySelector('.presentation');
 
-        // Vérifie si le projet actuel correspond au projet sélectionné
+        
         if (currentProject === project) {
-            // Si c'est le cas, rétablir le contenu initial de la présentation
+            
             presentationDiv.innerHTML = `<h1>Welcome.</h1><p>My name is Franck Zhang, I'm a full stack developer based in Paris, France. I am looking for an alternation in web development, data or cybersecurity for 2024-2025 (pace: 3 weeks in business / 1 week in school).<br><br> I am passionate about exploring the mysteries of the web, dissecting programming languages and experimenting with new ideas on my computer.p>`;
-            currentProject = ''; // Réinitialiser le projet actuel
+            currentProject = ''; 
         } else {
-            // Si ce n'est pas le cas, afficher la description du projet sélectionné
+            
             var description = getProjectDescription(project);
             if (presentationDiv) {
                 presentationDiv.innerHTML = `<h1>${project}</h1><p>${description}</p>`;
-                currentProject = project; // Mettre à jour le projet actuel
+                currentProject = project; 
             }
         }
     }
 
-    // Fonction pour récupérer la description du projet
+    
     function getProjectDescription(project) {
-        // Simulation de descriptions pour chaque projet
+        
         var descriptions = {
             achaka: "Description d'Achaka Conseils...",
             conquest: "Description de Conquest of Kingdoms...",
@@ -38,9 +28,6 @@ var currentYear = new Date().getFullYear();
             homepod: "Description de HomePod mini..."
         };
 
-        // Récupérer la description du projet en fonction de son nom
+        
         return descriptions[project] || "Aucune description disponible.";
     }
-
-
-    
