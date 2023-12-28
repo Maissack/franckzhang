12bae85ba4b0ba12f03cb1f8998c8c36aa16adcf
Maissack <franckzhangpro@gmail.com>
2023-12-28 12:06:26 +0100

    :sparkles: ADD switch language

--- js/btn_fr.js
+++ js/btn_fr.js
@@ -1,7 +1,7 @@
 function getSiteLink(project) {
     var siteLinks = {
         'Achaka Conseils': 'https://10.achakaconseils.com/?lang=fr',
-        'Conquest of Kingdoms': 'https://maissack.github.io/conquete_royaume',
+        'Conquête des Royaumes': 'https://maissack.github.io/conquete_royaume',
         'Mongoo': 'https://maissack.github.io/mongoo',
         'HomePod mini': 'https://maissack.github.io/homepod_mini',
     };
@@ -12,7 +12,7 @@ function getSiteLink(project) {
 function getGithubLink(project) {
     var githubLinks = {
         'Achaka Conseils': 'https://github.com/Code-Nam/Achaka-wordpress', 
-        'Conquest of Kingdoms': 'https://github.com/Maissack/conquete_royaume', 
+        'Conquête des Royaumes': 'https://github.com/Maissack/conquete_royaume', 
         'Mongoo': 'https://github.com/Maissack/mongoo', 
         'HomePod mini': 'https://github.com/Maissack/homepod_mini', 
     };
